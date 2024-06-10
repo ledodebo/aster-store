4ac6d54ba0076cb11b6339414aac086d67178c41
waleed mahmowd <34216752+ledodebo@users.noreply.github.com>
2024-06-10 11:42:47 +0300

    Update settings.py

--- aramatics/settings.py
+++ aramatics/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1w7rh-zljxt8v(8yc^-1%et=4$4v&ro%l!qafh&sn6x68pu69('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
